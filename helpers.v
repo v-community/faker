e715ebe5eeab20f8326ef1e34403013afa367fee
Vitor Oliveira <vbrazo@gmail.com>
2019-09-14 14:35:43 -0700

    Add Copyright

--- helpers.v
+++ helpers.v
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Vitor Oliveira. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 module helpers
 
 import rand
