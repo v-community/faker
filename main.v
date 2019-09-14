e2f0e8e7bab4707fe7348a96a117983b240bc684
Vitor Oliveira <vbrazo@gmail.com>
2019-09-14 03:15:42 -0700

    Println a random element of the country locale

--- main.v
+++ main.v
@@ -3,13 +3,20 @@
 // that can be found in the LICENSE file.
 
 import os
+import rand
+import time
 
 fn main() {
 	mut path := './data/address/country'
-	contents := os.read_file(path.trim_space()) or {
+	
+    contents := os.read_file(path.trim_space()) or {
 		println('failed to open $path')
 		return
 	}
+    
     lines := contents.split_into_lines()
-    println(lines)
+    length := lines.len
+
+    rand.seed(time.now().uni)
+    println(lines[rand.next(length-1)])
 }
