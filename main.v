1d86121c9f2dce4fe0c32ca083b6e4f50b876d25
Vitor Oliveira <vbrazo@gmail.com>
2019-09-14 01:48:29 -0700

    Add ability to read address in main.v and print the lines

---
+++ main.v
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Vitor Oliveira. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+import os
+
+fn main() {
+	mut path := './data/address/country'
+	contents := os.read_file(path.trim_space()) or {
+		println('failed to open $path')
+		return
+	}
+    lines := contents.split_into_lines()
+    println(lines)
+}
